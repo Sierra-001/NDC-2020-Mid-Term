77c7e5b6667af3e13cd4af8c22bf628682fd7531
Sierra-001A <48867097+Sierra-001@users.noreply.github.com>
2020-11-23 17:41:58 +0400

    added the main file

---
+++ Auction Company/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ReservePrice(9), index, NumBids(9), BuyNumID(9), SearchID As Integer
+        Dim Description(9), itemNumberID(9) As String
+        For index = 0 To 9
+            Console.WriteLine("Enter The Item: ")
+            itemNumberID(index) = CStr(Console.ReadLine)
+            Console.WriteLine("Enter The Item Description: ")
+            Description(index) = CStr(Console.ReadLine)
+            Console.WriteLine("Enter The Reserved Price for " & itemNumberID(index) & " : ")
+            ReservePrice(index) = CInt(Console.ReadLine)
+            NumBids(index) = 0
+            Console.Clear()
+        Next
+        For index = 0 To 9
+            Console.WriteLine("Item Number " & index + 1 & ": " & itemNumberID(index))
+            Console.WriteLine("Item Description " & index + 1 & ": " & Description(index))
+            Console.WriteLine("Reserved Price of " & itemNumberID(index) & ": " & ReservePrice(index))
+            Console.WriteLine()
+        Next
+        For index = 0 To 9
+            Console.WriteLine(itemNumberID(index) & " : " & index + 1)
+        Next
+        Console.WriteLine("Enter an item number for detail: ")
+        Console.Clear()
+        SearchID = CInt(Console.ReadLine)
+        For index = 0 To 9
+            If SearchID = itemNumberID(index + 1) Then
+                Console.WriteLine("Item Number : " & index + 1)
+                Console.WriteLine("Item : " & itemNumberID(index))
+                Console.WriteLine("Item Description : " & Description(index))
+                Console.WriteLine("Reserved Price : " & ReservePrice(index))
+                Console.WriteLine("Current Bids : " & NumBids(index))
+                Console.ReadKey()
+                Console.Clear()
+            End If
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Auction Company/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Auction Company")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Auction Company")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b8079f52-8ec4-48fc-bbd9-ec23c774c201")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
