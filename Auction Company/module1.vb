7aa74887091327956f6847c7bcb99fce69c62767
Sierra-001A <48867097+Sierra-001@users.noreply.github.com>
2020-12-03 11:04:13 +0400

    nothing to add
    Signed-off-by: Sierra-001A <48867097+Sierra-001@users.noreply.github.com>

--- Auction Company/module1.vb
+++ Auction Company/module1.vb
@@ -1,77 +1,43 @@
-Module module1
-    Sub Main()
-        Dim ReservePrice(9), NumBids(9), HighestBids(9), search, TempBids, index, limit As Integer
-        Dim Item(9), Description(9), decision, BuyID(limit) As String
+﻿Module Module1
 
-        For index = 0 To 2
-            Console.Write("Enter Item " & index + 1 & " Name :")
-            Item(index) = Console.ReadLine
-            Console.Write("Enter " & Item(index) & " Description :")
-            Description(index) = Console.ReadLine
-            Console.Write("Enter Reserved Price For " & Item(index) & " :")
-            ReservePrice(index) = Console.ReadLine
+    Sub Main()
+        Dim ReservePrice(9), index, NumBids(9), BuyNumID(9), SearchID As Integer
+        Dim Description(9), itemNumberID(9) As String
+        For index = 0 To 9
+            Console.WriteLine("Enter The Item: ")
+            itemNumberID(index) = CStr(Console.ReadLine)
+            Console.WriteLine("Enter The Item Description: ")
+            Description(index) = CStr(Console.ReadLine)
+            Console.WriteLine("Enter The Reserved Price for " & itemNumberID(index) & " : ")
+            ReservePrice(index) = CInt(Console.ReadLine)
             NumBids(index) = 0
-            HighestBids(index) = 0
             Console.Clear()
         Next
-        'Console.WriteLine()
-        Do
-            For index = 0 To 2
-                Console.WriteLine("Item " & index + 1 & ": " & Item(index))
-            Next
+        For index = 0 To 9
+            Console.WriteLine("Item Number " & index + 1 & ": " & itemNumberID(index))
+            Console.WriteLine("Item Description " & index + 1 & ": " & Description(index))
+            Console.WriteLine("Reserved Price of " & itemNumberID(index) & ": " & ReservePrice(index))
             Console.WriteLine()
-            'Console.WriteLine("do you want to place a bids y/n")
-            Console.Write("Enter The Item ID For More Details : ")
-            search = Console.ReadLine
-            For index = 0 To 2
-                If (search = index + 1) Then
-                    Console.WriteLine("Item " & index + 1 & ": " & Item(index))
-                    Console.WriteLine(Item(index) & " Description :" & Description(index))
-                    Console.WriteLine(Item(index) & " Reserve Price : " & ReservePrice(index))
-                    Console.WriteLine("Current Bids: " & NumBids(index))
-                    Console.WriteLine("Highest Bids: " & HighestBids(index))
-                    Console.WriteLine()
-                    'If (index = 9) Then
-                    '    Console.WriteLine("press enter to exit!")
-                    'End If
-                    Console.WriteLine("Do You Want To Place a Bids? y/n")
-                    decision = Console.ReadLine
-                    If ((decision = "y") Or (decision = "Y")) Then
-                        Console.Write("Enter Buyer ID : ")
-                        BuyID(limit) = Console.ReadLine
-                        Console.Write("Enter Bids For " & Item(index) & " :")
-                        TempBids = Console.ReadLine
-                        If (TempBids > HighestBids(index)) Then
-                            HighestBids(index) = TempBids
-                            NumBids(index) = NumBids(index) + 1
-                            Console.Clear()
-                            Console.WriteLine("Information For Item " & Item(index) & " have been changed!")
-                        End If
-                    End If
-                End If
-            Next
-            For index = 0 To 2
-                Console.WriteLine("Item " & index + 1 & ": " & Item(index))
-                Console.WriteLine(Item(index) & " Description :" & Description(index))
-                Console.WriteLine(Item(index) & " Reserve Price : " & ReservePrice(index))
-                Console.WriteLine("Current Bids: " & NumBids(index))
-                Console.WriteLine("Highest Bids: " & HighestBids(index))
-                Console.WriteLine()
-            Next
-            Console.WriteLine()
-            Console.WriteLine("press any key to continue...")
-            Console.ReadKey()
-            Console.Clear()
-            Console.WriteLine("You you want to continue? y/n")
-            decision = Console.ReadLine
-        Loop Until ((decision = "n") Or (decision = "N"))
-        'final bids -- 10% off
-        '
-
-
-
-
-
+        Next
+        For index = 0 To 9
+            Console.WriteLine(itemNumberID(index) & " : " & index + 1)
+        Next
+        Console.WriteLine("Enter an item number for detail: ")
+        Console.Clear()
+        SearchID = CInt(Console.ReadLine)
+        For index = 0 To 9
+            If SearchID = itemNumberID(index + 1) Then
+                Console.WriteLine("Item Number : " & index + 1)
+                Console.WriteLine("Item : " & itemNumberID(index))
+                Console.WriteLine("Item Description : " & Description(index))
+                Console.WriteLine("Reserved Price : " & ReservePrice(index))
+                Console.WriteLine("Current Bids : " & NumBids(index))
+                Console.ReadKey()
+                Console.Clear()
+            End If
+        Next
         Console.ReadKey()
+
     End Sub
+
 End Module
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Computer-Science-2020-
 "# NDC-Computer-MID_TERM_2020" 
+Note: the folder auction company[audited] is a source code downloaded from the internet and the code inside is doing exactly what were required but have a lot of error and need to be edited and the final result will be merge with auction company[audited]
